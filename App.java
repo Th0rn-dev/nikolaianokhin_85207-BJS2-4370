db1141df6de040224b3d0ebbad2ef7567ee495c0
Th0rn-dev <solnek33@gmail.com>
2024-04-04 18:27:45 +0300

    Revert "commit for move to feature-c branch"
    This reverts commit ec6721489c53f5191344fc66919f217e4e841275.

--- App.java
+++ App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Открытие магазина ожидается через 2 месяца");
-        System.out.println("В магазине будет большой ассортимент книг жанра - Фантастика");
     }
 }
